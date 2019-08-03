3a2dc98ee5e75e80fdcd44bc660a550e7156db20
Ryan Mokarian <mokarian.m@gmail.com>
2019-08-03 16:59:44 -0400

    placeholder for update logic

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "mysql";
-// $dbname = "Test_WarmUp_Project";
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+$dbname = "Test_WarmUp_Project";
 
-$servername = "qqc5531.encs.concordia.ca";
-$username = "qqc55311";
-$password = "p1usp1us";
-$dbname = "qqc55311"; 
+// $servername = "qqc5531.encs.concordia.ca";
+// $username = "qqc55311";
+// $password = "p1usp1us";
+// $dbname = "qqc55311"; 
