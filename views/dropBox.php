1f72ef9b0c40ca6be09990a8f337a746c7902e91
Mani <mani.ahmadi@ssense.com>
2019-08-03 14:58:46 -0400

    bad dr

--- views/dropBox.php
+++ views/dropBox.php
@@ -1,16 +1,20 @@
+<?php
+$tables = getAllTables($conn);
+?>
+
 <form action="" method="get">
        <fieldset>
           <legend>Select a table</legend>
           <p>
             <label>Tables</label>
             <select id = "table_name" name='table_name'>
-               <option value = "Student">Student</option>
-               <option value = "Class">Class</option>
-            </select>
+               <?php
+                  foreach ($tables as $table) {
+                     echo "<option value = \"{$table}\">{$table}</option>";
+                  }
+               ?>
+               </select>
           </p>
           <input type="submit" value="Submit">
        </fieldset>
 </form>
-<?php
-
-print_r(getAllTables($conn));
