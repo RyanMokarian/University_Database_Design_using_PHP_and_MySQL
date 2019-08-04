914e4c5deaa0002eef6427a89f5898f33623d23e
Mani <mani.ahmadi@ssense.com>
2019-08-04 16:23:48 -0400

    move things around

--- views/create.php
+++ views/create.php
@@ -1,11 +1,17 @@
 <?php
+echo "<table>";
+echo '<tr>';
 echo '<form action="" method="post">';
 echo '<input type="hidden" name="__insert" value=""><br>';
 foreach ($columns as $column) {
 	//echo $column;
+	echo "<td>";
 	echo '<input type="text" name="'. $column. '" value="">';
+	echo "</td>";
 }
-echo '<input type="submit" value="Submit"></form>';  
+echo '<input type="submit" value="Submit"></form>';
+echo '</tr>';
+echo "</table>";
 $postData = $_POST;
 //process insert by getting the input from $POST
 if (!empty($_POST) && isset($_POST['__insert'])) {
