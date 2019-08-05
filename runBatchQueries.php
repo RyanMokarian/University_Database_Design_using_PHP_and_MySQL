a00aaf0fa45365f0ea5799b118fa6c44c53be893
Mani <mani.ahmadi@ssense.com>
2019-08-04 22:14:06 -0400

    batch script

--- runBatchQueries.php
+++ runBatchQueries.php
@@ -8,17 +8,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ($fh = fopen('theBigInserter.txt', 'r')) {
-    while (!feof($fh)) {
-        $line = fgets($fh);
-        echo $line;
-        $sql_Insert = $line;
-    
-        if ($conn->query($sql_Insert) === TRUE) {
-            return "New record created successfully";
-        } else {
-            return "Error: " . $sql_Insert . "<br>" . $conn->error;
-        }
-    }
-    fclose($fh);
+$count = 0;
+$fn = fopen("theBigInserter.txt","r");
+  
+while(! feof($fn))  {
+	$result = fgets($fn);
+	echo $result;
+	$r = $conn->query($result);
+	
+	if (!$r) {
+		echo ($conn->error);
+	}
 }
+
+fclose($fn);
